465c87fa899159c3d09099d8429f6ea52ea73f57
albadria <ahmed.albadri25@gmail.com>
2015-05-14 20:50:14 -0400

    Put the text in the date wheel layout.  Fixed a few issues with the wheel numbers.  Updating properly when each property changes

--- wheel/src/kankan/wheel/widget/DateTimePicker.java
+++ wheel/src/kankan/wheel/widget/DateTimePicker.java
@@ -37,6 +37,8 @@ public class DateTimePicker implements Serializable {
     public static final Integer[] EMPTY_INTEGER_OBJECT_ARRAY = new Integer[0];
     private transient Animation _fadeInAnimation;
     private transient Animation _fadeOutAnimation;
+    private transient View _timeFrameDateWheelView;
+    private transient TextView _timeFrameDateTextView;
     private transient WheelView _dayMonth;
     private transient WheelView _month;
     private transient WheelView _year;
@@ -44,26 +46,43 @@ public class DateTimePicker implements Serializable {
     private transient WheelView _hour;
     private transient WheelView _minute;
     private transient WheelView _period;
-    private transient View _timeFrameView;
-    private Calendar _calendar;
+    private Calendar _initialCalendar;
+    private Calendar _resultCalendar;
+    private static final SimpleDateFormat dateTimeFormatter = new SimpleDateFormat("MMM dd, yyyy h:mm a", Locale.US);
+    private static final SimpleDateFormat allDayDateFormatter = new SimpleDateFormat("EEE, MMM dd, yyyy", Locale.US);
+    private boolean _timeFrameHeaderClicked;
     private boolean _allDay;
-    private static final SimpleDateFormat stringDateFormatter = new SimpleDateFormat("EEE, MMM dd, yyyy");
-    private boolean _visible;
+    private int _additionalYearsToPopulate = 1;
 
-    public DateTimePicker(final Activity activity, View timeFrameView, Calendar calendar) {
+    public DateTimePicker(final Activity activity, View timeFrameView, Calendar calendar, String header, int numberOfAdditionalYearsToPopulate) {
         if (activity == null)
             return;
 
-        _timeFrameView = timeFrameView;
+        View timeFrameHeaderView = timeFrameView.findViewById(R.id.time_frame_header);
+        TextView timeFrameHeaderTextView = (TextView) timeFrameView.findViewById(R.id.time_frame_header_text);
+        _dayMonth = (WheelView) timeFrameView.findViewById(R.id.day_month);
+        _month = (WheelView) timeFrameView.findViewById(R.id.month);
+        _year = (WheelView) timeFrameView.findViewById(R.id.year);
+        _day = (WheelView) timeFrameView.findViewById(R.id.day);
+        _hour = (WheelView) timeFrameView.findViewById(R.id.hour);
+        _minute = (WheelView) timeFrameView.findViewById(R.id.minute);
+        _period = (WheelView) timeFrameView.findViewById(R.id.period);
+        _timeFrameDateTextView = (TextView) timeFrameView.findViewById(R.id.time_frame_date_text);
+        _timeFrameDateWheelView = timeFrameView.findViewById(R.id.time_frame_date_wheel);
+        _additionalYearsToPopulate = numberOfAdditionalYearsToPopulate;
 
-        _dayMonth = (WheelView) _timeFrameView.findViewById(R.id.day_month);
-        _month = (WheelView) _timeFrameView.findViewById(R.id.month);
-        _year = (WheelView) _timeFrameView.findViewById(R.id.year);
-        _day = (WheelView) _timeFrameView.findViewById(R.id.day);
-        _hour = (WheelView) _timeFrameView.findViewById(R.id.hour);
-        _minute = (WheelView) _timeFrameView.findViewById(R.id.minute);
-        _period = (WheelView) _timeFrameView.findViewById(R.id.period);
-        _calendar = calendar;
+        timeFrameHeaderTextView.setText(header);
+        _timeFrameDateWheelView.setVisibility(View.GONE);
+
+        _initialCalendar = (Calendar) calendar.clone();
+        _initialCalendar.set(Calendar.HOUR, 12);
+        _initialCalendar.set(Calendar.MINUTE, 0);
+        _initialCalendar.set(Calendar.AM_PM, 0);
+
+        _resultCalendar = (Calendar) calendar.clone();
+        _resultCalendar.set(Calendar.HOUR, 12);
+        _resultCalendar.set(Calendar.MINUTE, 0);
+        _initialCalendar.set(Calendar.AM_PM, 0);
 
         OnWheelChangedListener dayListener = new OnWheelChangedListener() {
             public void onChanged(WheelView wheel, int oldValue, int newValue) {
@@ -85,36 +104,98 @@ public class DateTimePicker implements Serializable {
                 updateTime(_period);
             }
         };
-        
+
         // month
-        int curMonth = _calendar.get(Calendar.MONTH);
+        int curMonth = _initialCalendar.get(Calendar.MONTH);
         String months[] = activity.getResources().getStringArray(R.array.month_array);
         _month.setViewAdapter(new DateArrayAdapter(activity, months, curMonth));
         _month.setCurrentItem(curMonth);
         _month.setDrawShadows(false);
         _month.addChangingListener(dayListener);
+        _month.addChangingListener(new OnWheelChangedListener() {
+            @Override
+            public void onChanged(WheelView wheel, int oldValue, int newValue) {
+                _resultCalendar.set(Calendar.MONTH, newValue);
+                updateTimeFrameText();
+            }
+
+            @Override
+            public void onCycled(WheelView wheelView, CycleType cycleType) {
+
+            }
+        });
 
         // day month
-        int dayOfMonth = _calendar.get(Calendar.DAY_OF_MONTH);
+        int dayOfMonth = _initialCalendar.get(Calendar.DAY_OF_MONTH);
         String dayMonthArray[] = generateDayMonthArray();
         _dayMonth.setDrawShadows(false);
         _dayMonth.setViewAdapter(new ArrayWheelAdapter<>(activity, dayMonthArray, 16));
+        _dayMonth.addChangingListener(new OnWheelChangedListener() {
+            @Override
+            public void onChanged(WheelView wheel, int oldValue, int newValue) {
+                _resultCalendar.set(Calendar.DAY_OF_YEAR, _initialCalendar.get(Calendar.DAY_OF_YEAR));
+                _resultCalendar.set(Calendar.YEAR, _initialCalendar.get(Calendar.YEAR));
+                _resultCalendar.add(Calendar.DAY_OF_YEAR, newValue);
+                updateTimeFrameText();
+            }
+
+            @Override
+            public void onCycled(WheelView wheelView, CycleType cycleType) {
+
+            }
+        });
 
         // year
-        int curYear = _calendar.get(Calendar.YEAR);
-        _year.setViewAdapter(new DateNumericAdapter(activity, curYear, curYear + 10, 0));
+        int curYear = _initialCalendar.get(Calendar.YEAR);
+        _year.setViewAdapter(new DateNumericAdapter(activity, curYear, curYear + _additionalYearsToPopulate, 0));
         _year.setCurrentItem(curYear);
         _year.setDrawShadows(false);
         _year.addChangingListener(dayListener);
+        _year.addChangingListener(new OnWheelChangedListener() {
+            @Override
+            public void onChanged(WheelView wheel, int oldValue, int newValue) {
+                _resultCalendar.set(Calendar.YEAR, _initialCalendar.get(Calendar.YEAR) + newValue);
+                updateTimeFrameText();
+            }
+
+            @Override
+            public void onCycled(WheelView wheelView, CycleType cycleType) {
+
+            }
+        });
 
         // day
         updateDays(activity, _year, _month, _day);
         _day.setCurrentItem(dayOfMonth - 1);
         _day.setDrawShadows(false);
+        _day.addChangingListener(new OnWheelChangedListener() {
+            @Override
+            public void onChanged(WheelView wheel, int oldValue, int newValue) {
+                _resultCalendar.set(Calendar.DAY_OF_MONTH, newValue + 1);
+                updateTimeFrameText();
+            }
+
+            @Override
+            public void onCycled(WheelView wheelView, CycleType cycleType) {
+
+            }
+        });
 
         // period
         _period.setViewAdapter(new ArrayWheelAdapter<>(activity, activity.getResources().getStringArray(R.array.period_array), 16));
         _period.setDrawShadows(false);
+        _period.addChangingListener(new OnWheelChangedListener() {
+            @Override
+            public void onChanged(WheelView wheel, int oldValue, int newValue) {
+                _resultCalendar.set(Calendar.AM_PM, newValue);
+                updateTimeFrameText();
+            }
+
+            @Override
+            public void onCycled(WheelView wheelView, CycleType cycleType) {
+
+            }
+        });
 
         // hour
         _hour.setViewAdapter(new ArrayWheelAdapter<>(activity, integerToObject(activity.getResources().getIntArray(R.array.hour_array)), 16));
@@ -122,12 +203,36 @@ public class DateTimePicker implements Serializable {
         _hour.setCyclic(true);
         _hour.setDrawShadows(false);
         _hour.addChangingListener(timeListener);
+        _hour.addChangingListener(new OnWheelChangedListener() {
+            @Override
+            public void onChanged(WheelView wheel, int oldValue, int newValue) {
+                _resultCalendar.set(Calendar.HOUR, newValue);
+                updateTimeFrameText();
+            }
+
+            @Override
+            public void onCycled(WheelView wheelView, CycleType cycleType) {
+
+            }
+        });
 
         // minute
-        _minute.setViewAdapter(new DateNumericAdapter(activity, 0, 59, 1, 0));
+        _minute.setViewAdapter(new DateNumericAdapter(activity, 0, 59, 5, 0));
         _minute.setCurrentItem(curYear);
         _minute.setCyclic(true);
         _minute.setDrawShadows(false);
+        _minute.addChangingListener(new OnWheelChangedListener() {
+            @Override
+            public void onChanged(WheelView wheel, int oldValue, int newValue) {
+                _resultCalendar.set(Calendar.MINUTE, newValue);
+                updateTimeFrameText();
+            }
+
+            @Override
+            public void onCycled(WheelView wheelView, CycleType cycleType) {
+
+            }
+        });
 
         _fadeInAnimation = AnimationUtils.loadAnimation(activity, R.anim.fadein);
 
@@ -140,7 +245,7 @@ public class DateTimePicker implements Serializable {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                _timeFrameView.setVisibility(View.GONE);
+                _timeFrameDateWheelView.setVisibility(View.GONE);
             }
 
             @Override
@@ -148,10 +253,45 @@ public class DateTimePicker implements Serializable {
 
             }
         });
+
+        timeFrameHeaderView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                _timeFrameHeaderClicked = !_timeFrameHeaderClicked;
+                slide(_timeFrameHeaderClicked);
+                _timeFrameDateTextView.setTextColor(_timeFrameHeaderClicked ? activity.getResources().getColor(R.color.color_red) : activity.getResources().getColor(R.color.color_black));
+                updateTimeFrameText();
+
+            }
+        });
+
+        updateTimeFrameText();
     }
 
     public void setAllDay(boolean allDay){
         _allDay = allDay;
+        updateViews(allDay);
+    }
+
+    public void slide(final boolean visible) {
+        if (visible)
+            _timeFrameDateWheelView.setVisibility(View.VISIBLE);
+
+        _timeFrameDateWheelView.startAnimation(visible ? _fadeInAnimation : _fadeOutAnimation);
+    }
+
+    private void updateViews(boolean allDay) {
+        int yearDifference = _resultCalendar.get(Calendar.YEAR) - _initialCalendar.get(Calendar.YEAR);
+
+        if (allDay){
+            _year.setCurrentItem(yearDifference);
+            _month.setCurrentItem(_resultCalendar.get(Calendar.MONTH));
+            _day.setCurrentItem(_resultCalendar.get(Calendar.DAY_OF_MONTH) - 1);
+        }
+        else{
+            _dayMonth.setCurrentItem(_resultCalendar.get(Calendar.DAY_OF_YEAR) - _initialCalendar.get(Calendar.DAY_OF_YEAR) + yearDifference * 365);
+        }
+
         _dayMonth.setVisibility(allDay ? View.GONE : View.VISIBLE);
         _hour.setVisibility(allDay ? View.GONE : View.VISIBLE);
         _minute.setVisibility(allDay ? View.GONE : View.VISIBLE);
@@ -159,36 +299,33 @@ public class DateTimePicker implements Serializable {
         _day.setVisibility(allDay ? View.VISIBLE : View.GONE);
         _month.setVisibility(allDay ? View.VISIBLE : View.GONE);
         _year.setVisibility(allDay ? View.VISIBLE : View.GONE);
-    }
-
-    public void slide(final boolean visible) {
-        if (visible)
-            _timeFrameView.setVisibility(View.VISIBLE);
-        
-        _timeFrameView.startAnimation(visible ? _fadeInAnimation : _fadeOutAnimation);
+        updateTimeFrameText();
     }
 
     private String[] generateDayMonthArray() {
-        String[] dayMonthArray = new String[365];
-        Calendar newCalendar = Calendar.getInstance();
+        int finalYear = _initialCalendar.get(Calendar.YEAR) + _additionalYearsToPopulate;
 
-        SimpleDateFormat formatter = new SimpleDateFormat("EEE MMM dd");
+        Calendar newCalendar = (Calendar) _initialCalendar.clone();
+        SimpleDateFormat formatter = new SimpleDateFormat("EEE MMM dd", Locale.US);
 
-        dayMonthArray[0] = formatter.format(newCalendar.getTime());
 
-        for (int i = 1; i < 365; i++){
+        ArrayList<String> dayMonthArray = new ArrayList<String>();
+
+        // until we have progressed until the final year, keep adding days
+        do {
+            dayMonthArray.add(formatter.format(newCalendar.getTime()));
             newCalendar.add(Calendar.DAY_OF_YEAR, 1);
-            dayMonthArray[i] = formatter.format(newCalendar.getTime());
         }
+        while ((newCalendar.get(Calendar.YEAR) <= finalYear));
 
-        return dayMonthArray;
+        return dayMonthArray.toArray(new String[dayMonthArray.size()]);
     }
 
     /**
      * Updates day wheel. Sets max days according to selected month and year
      */
     private void updateDays(Activity activity, WheelView year, WheelView month, WheelView day) {
-        Calendar calendar = Calendar.getInstance();
+        Calendar calendar = (Calendar) _initialCalendar.clone();
         calendar.set(Calendar.YEAR, calendar.get(Calendar.YEAR) + year.getCurrentItem());
         calendar.set(Calendar.MONTH, month.getCurrentItem());
 
@@ -205,26 +342,6 @@ public class DateTimePicker implements Serializable {
         period.setCurrentItem((period.getCurrentItem() + 1) % 2, true);
     }
 
-    private int getSelectedYear(){
-        return _calendar.get(Calendar.YEAR) + _year.getCurrentItem();
-    }
-
-    private int getSelectedMonth(){
-        return _month.getCurrentItem();
-    }
-
-    private int getSelectedDay(){
-        return _day.getCurrentItem();
-    }
-
-    private int getSelectedHour(){
-        return _hour.getCurrentItem();
-    }
-
-    private int getSelectedMinute(){
-        return _minute.getCurrentItem();
-    }
-
     private static Integer[] integerToObject(int[] array) {
         if (array == null) {
             return null;
@@ -238,11 +355,12 @@ public class DateTimePicker implements Serializable {
         return result;
     }
 
+    private void updateTimeFrameText() {
+        _timeFrameDateTextView.setText(this.toString());
+    }
+
     @Override
     public String toString() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.clear();
-        calendar.set(getSelectedYear(), getSelectedMonth(), getSelectedDay(), getSelectedHour(), getSelectedMinute());
-        return stringDateFormatter.format(calendar.getTime());
+        return _allDay ? allDayDateFormatter.format(_resultCalendar.getTime()) : dateTimeFormatter.format(_resultCalendar.getTime());
     }
 }
